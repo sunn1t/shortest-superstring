8d701f38bccf778cbac6ad2a25e4967173acd515
sunnut <sunnut@localhost.localdomain>
2020-04-15 01:11:29 +0300

    Folder structure changed, CMakeLists.txt changed

---
+++ src/graph.h
@@ -0,0 +1,36 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <cstddef>
+
+struct Path {
+public:
+    size_t *v_sequence;
+    size_t n;
+};
+
+class Graph {
+public:
+    Graph(size_t n_vertex);
+    Graph(const Graph &old);
+    Graph &operator = (const Graph &old);
+
+
+    void AddEdge(size_t from, size_t to, size_t length);
+
+    size_t ** GetMatrix() const;
+    size_t * GetEdgesFrom(const size_t from) const;
+    size_t GetSize() const;
+    size_t GetLength(Path const &path) const;
+
+    void PrintAdjMatrix() const;
+
+    ~Graph();
+
+private:
+    size_t n_vertex;
+    size_t ** adj_matrix;
+};
+
+#endif // GRAPH_H
+
---
+++ test/tests.cpp
@@ -0,0 +1,44 @@
+#ifndef TST_STRTEST_H
+#define TST_STRTEST_H
+
+#include <gtest/gtest.h>
+#include <algorithms.h>
+
+//using namespace testing;
+
+static std::string test_set1[] = {"aaabb", "bccccc", "ccdd", "dddee", "eee"};
+static std::string test_set2[] = {"mmsstQ", "JccdddgZ", "QaajjuuuasdfJ", "ZZZZ", "Zhh"};
+static std::string test_set3[] = {"Abc", "Bcd", "Cde"};
+static std::string test_set4[] = {"bLLSeiidofeiobdleids", "UdiOOqieJdibLLSeiido"};
+static std::string test_set5[] = {"AubSIdle", "GoicRaiYY", "oicRaiYYAubSIdl"};
+static std::string test_set6[] = {"cde", "abc", "eab", "fgh", "ghf", "hed"};
+static std::string test_set7[] = {"waiwa", "digoe", "socib", "dsoec", "isdie", "cibis"};
+static std::string test_set8[] = {"urobsb", "ionnao", "pmequd",
+                                  "xxpmtu", "nrmseh", "cbivkc"
+                                  "cdcsfx", "xcxkvk", "qbsgcq"};
+static std::string test_set9[] = {"gfffdddfafga", "gcedcfcgbdbc",
+                                 "bffccbgcfagd", "bcedgfcebdfc",
+                                 "cfdffegccdgg", "fccbeefcabff"};
+static std::string test_set10[] = {"hnem", "fnhg", "liid", "mfai", "nddg",
+                                  "fkig", "almj", "bine", "iajj", "janh"};
+
+TEST(ExactTest, Simple)
+{
+    EXPECT_EQ(ExactSolution(5, test_set1), "aaabbcccccdddeee");
+    EXPECT_EQ(ExactSolution(5, test_set2), "mmsstQaajjuuuasdfJccdddgZZZZhh");
+    EXPECT_EQ(ExactSolution(3, test_set3), "CdeBcdAbc");
+    EXPECT_EQ(ExactSolution(2, test_set4), "UdiOOqieJdibLLSeiidofeiobdleids");
+    EXPECT_EQ(ExactSolution(3, test_set5), "GoicRaiYYAubSIdle");
+}
+
+TEST(ApprTest, Simple)
+{
+    EXPECT_EQ(AlgorithmBJLTY(6, test_set6), "cdeabcfghfhed");
+    EXPECT_EQ(AlgorithmBJLTY(6, test_set7), "waiwadigoesocibisdiedsoec");
+    EXPECT_EQ(AlgorithmBJLTY(9, test_set8), "urobsbionnaopmequdnrmsehcbivkccdcsfxxpmtuxcxkvkqbsgcqgfffdddfafga");
+    EXPECT_EQ(AlgorithmBJLTY(6, test_set9), "gfffdddfafgagcedcfcgbdbcedgfcebdfccbeefcabffccbgcfagdcfdffegccdgg");
+    EXPECT_EQ(AlgorithmBJLTY(10, test_set10), "hnemfaiajjbinealmjanhfnhgliidnddgfkig");
+}
+
+#endif // TST_STRTEST_H
+
